000abd01026d0d6e76f7c767cf5b4b8a89be570a
Ubuntu <ubuntu@ip-172-31-13-250.ec2.internal>
2016-05-17 20:55:23 +0000

    Removed unneeded security script

--- groovy/security.groovy
+++
@@ -1,8 +0,0 @@
-import jenkins.model.*
-import hudson.security.*
- 
-def instance = Jenkins.getInstance()
-//def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-//instance.setSecurityRealm(hudsonRealm)
-instance.setSlaveAgentPort(50001)
-instance.save()
